9f452df19e7f6a5c24670a41ab7f077f48e8e7ab
vhadas <h56781602@gmail.com>
2024-10-10 09:34:35 +0300

    Project upload

---
+++ model.js
@@ -0,0 +1,122 @@
+const GBooks = [
+    {
+        "name": "השקעות לעצלנים",
+        "price": 29,
+        "rating": 9,
+        "image": "https://www.steimatzky.co.il/pub/media/catalog/product/cache/7de3bd51a14570365ea4a3b182c00065/0/1/011251352-164502225375980.jpg"
+    },
+    {
+        "name": "סטילר",
+        "price": 36,
+        "rating": 10,
+        "image": "https://www.steimatzky.co.il/pub/media/catalog/product/cache/7de3bd51a14570365ea4a3b182c00065/0/1/012193199-171803023162210.jpg"
+    },
+    {
+        "name": "יום אחד באוקטובר",
+        "price": 78.40,
+        "rating": 8,
+        "image": "https://www.steimatzky.co.il/pub/media/catalog/product/cache/7de3bd51a14570365ea4a3b182c00065/0/1/011620329-1716458513133269.jpg"
+    },
+    {
+        "name": "המלך דוד",
+        "price": 59.90,
+        "rating": 9,
+        "image": "https://www.steimatzky.co.il/pub/media/catalog/product/cache/7de3bd51a14570365ea4a3b182c00065/0/1/012193200-171801153051415.jpg"
+    },
+    {
+        "name": "עמנואל",
+        "price": 59.90,
+        "rating": 7,
+        "image": "https://www.steimatzky.co.il/pub/media/catalog/product/cache/7de3bd51a14570365ea4a3b182c00065/0/1/012193201-171801175717272.jpg"
+    },
+    {
+        "name": "שעת הזאבים",
+        "price": 78.40,
+        "rating": 10,
+        "image": "https://www.steimatzky.co.il/pub/media/catalog/product/cache/7de3bd51a14570365ea4a3b182c00065/0/1/012292494-17228444431335320.jpg"
+    },
+    {
+        "name": "הכלה האחרת",
+        "price": 78.40,
+        "rating": 8,
+        "image": "https://www.steimatzky.co.il/pub/media/catalog/product/cache/7de3bd51a14570365ea4a3b182c00065/0/1/011415656-1715079629746349.jpg"
+    },
+    {
+        "name": "סולם גנבים",
+        "price": 78.40,
+        "rating": 9,
+        "image": "https://www.steimatzky.co.il/011374943"
+    },
+    {
+        "name": "עד שליבך יתנגן",
+        "price": 94.40,
+        "rating": 8,
+        "image": "https://www.steimatzky.co.il/011374943"
+    },
+    {
+        "name": "המדריך לשמחת חיים",
+        "price": 78.40,
+        "rating": 9,
+        "image": "https://upload.wikimedia.org/wikipedia/commons/0/0f/Jane_Eyre_cover.jpg"
+    },
+    {
+        "name": "Brave New World",
+        "price": 13.99,
+        "rating": 9,
+        "image": "https://www.steimatzky.co.il/pub/media/catalog/product/cache/7de3bd51a14570365ea4a3b182c00065/0/1/014561888-16892459922473356.jpg"
+    },
+    {
+        "name": "אוטוקורקט",
+        "price": 78.40,
+        "rating": 8,
+        "image": "https://www.steimatzky.co.il/pub/media/catalog/product/cache/7de3bd51a14570365ea4a3b182c00065/0/1/010033364-171507157252197.jpg"
+    },
+    {
+        "name": "ענין משפחתי",
+        "price": 98,
+        "rating": 8,
+        "image": "https://www.booknet.co.il/images/site/products/opt/243638_174_auto.jpg"
+    },
+    {
+        "name": "גינס שיאי עולם 2025",
+        "price": 198,
+        "rating": 8,
+        "image": "https://www.booknet.co.il/Images/Site/Products/3800018442.jpg"
+    },
+    {
+        "name": "ורדים אבודים",
+        "price": 39.90,
+        "rating": 9,
+        "image": "https://www.booknet.co.il/Images/Site/Products/62512016528.jpg"
+    },
+    {
+        "name": "נקסוס",
+        "price": 106,
+        "rating": 7,
+        "image": "https://www.booknet.co.il/Images/Site/Products/org/100069990.jpg"
+    },
+    {
+        "name": "קו התלם",
+        "price": 98,
+        "rating": 10,
+        "image": "https://www.booknet.co.il/Images/Site/Products/36200070550.jpg"
+    },
+    {
+        "name": "מן האפר",
+        "price": 102,
+        "rating": 8,
+        "image": "https://www.booknet.co.il/images/site/products/opt/243166_174_auto.jpg"
+    },
+    {
+        "name": "חופשה צרפתית",
+        "price": 98,
+        "rating": 9,
+        "image": "https://www.booknet.co.il/images/site/products/opt/243468_174_auto.jpg"
+    },
+    {
+        "name": "איתנו זה נגמר",
+        "price": 39.90,
+        "rating": 7,
+        "image": "https://www.booknet.co.il/images/site/products/opt/241256_174_auto.jpg"
+    }
+]
